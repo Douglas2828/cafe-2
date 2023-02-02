9c15823f331663f60820bdad4026d8c307f417b7
Douglas2828 <118612832+Douglas2828@users.noreply.github.com>
2023-02-01 23:10:03 -0300

    CaféDnc

---
+++ index.js
@@ -0,0 +1,78 @@
+
+    function acao(){
+
+    let modal = document.querySelector('.modal1')
+
+
+    modal.style.display ='block';
+}
+
+    function fechar(){
+
+    let modal = document.querySelector('.modal1')
+
+
+    modal.style.display ='none';
+}
+
+
+    function mostrardiv(){
+
+    let btn0 = document.querySelector('.buttonhide1')
+
+    btn0.style.display ='block';
+}
+
+    function sumirdiv()
+{
+    let btn0 = document.querySelector('.buttonhide1')
+
+    btn0.style.display ='none';
+
+}
+
+
+    function mostrardiv2(){
+
+    let btn2 = document.querySelector('.buttonhide2')
+    
+    btn2.style.display ='block';
+}
+    
+    function sumirdiv2()
+{
+    let btn2 = document.querySelector('.buttonhide2')
+    
+    btn2.style.display ='none';
+}
+
+
+    function mostrardiv3(){
+
+    let btn3 = document.querySelector('.buttonhide3')
+    
+    btn3.style.display ='block';
+}
+    
+    function sumirdiv3()
+{
+    let btn3 = document.querySelector('.buttonhide3')
+    
+    btn3.style.display ='none';
+}
+
+
+function mostrardiv4(){
+    
+    let btn4 =document.querySelector('.buttonhide4')
+
+    btn4.style.display ='block';
+}  
+
+function sumirdiv4(){
+    
+    let btn4 =document.querySelector('.buttonhide4')
+
+    btn4.style.display ='none';
+
+}
